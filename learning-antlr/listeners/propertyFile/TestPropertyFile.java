d54ef2d8fadfaaeb703f33daa66d8c37ffec3c3b
weizhiwei <zhiwei.wei@bintools.cn>
2022-01-25 15:46:53 +0800

    TestPropertyFile recorded

--- learning-antlr/listeners/propertyFile/TestPropertyFile.java
+++ learning-antlr/listeners/propertyFile/TestPropertyFile.java
@@ -1,22 +1,21 @@
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-import org.antlr.v4.runtime.Token;
-import java.utils.*;
-import java.util.Map;
-import org.antlr.v4.misc.*;
+import org.antlr.v4.misc.OrderedHashMap;
+import java.util.*;
 public class TestPropertyFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         ANTLRInputStream input = new ANTLRInputStream(System.in);
         PropertyFileLexer lexer = new PropertyFileLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         PropertyFilePrinter parser = new PropertyFilePrinter(tokens);
-        ParseTree tree = parser.file(); // launch our special version of the parser
-        // create a standard ANTLR parse tree walker
+        ParseTree tree= parser.file(); // launch our special version of the parser
         ParseTreeWalker walker = new ParseTreeWalker();
 // create listener then feed to walker
         PropertyFileLoader loader = new PropertyFileLoader();
         walker.walk(loader, tree); // walk parse tree
         System.out.println(loader.props); // print results
     }
+
 
     public static class PropertyFileLoader extends PropertyFileBaseListener {
         Map<String,String> props = new OrderedHashMap<String, String>();
@@ -26,15 +25,31 @@ public class TestPropertyFile {
             props.put(id, value);
         }
     }
+
 }
 
 
+
+
+
+
+
 class PropertyFilePrinter extends PropertyFileParser {
+    public PropertyFilePrinter(TokenStream input) {
+        super(input);
+    }
     void defineProperty(Token name, Token value) {
         System.out.println(name.getText()+"="+value.getText());
     }
 }
 
 
-
-
+class PropertyFileLoader extends PropertyFileParser {
+    public PropertyFileLoader(TokenStream input) {
+        super(input);
+    }
+    Map<String,String> props = new OrderedHashMap<String, String>();
+    void defineProperty(Token name, Token value) {
+        props.put(name.getText(), value.getText());
+    }
+}
